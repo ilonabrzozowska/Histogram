0051b3da4ec8f707cc6037f5a84397cc7b403b86
jszum <jaroslaw.szumega@gmail.com>
2015-12-06 00:52:08 +0100

    Python script calling octave functions - using oct2py.

--- hist.py
+++ hist.py
@@ -3,20 +3,42 @@
 import Image
 import matplotlib.pyplot as plt
 import sys
+import os
 import numpy as np
+import oct2py
+import Image
+import cv2
 
+def myread(file):
+	img = cv2.imread(file)
+	img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+	return img
+
+def rgb2yuv(image):opencv
+	return
 
 
 if __name__ == "__main__":
 
 	file = sys.argv[1]
-	img = Image.open(file)
-	gray = img.convert('LA')
-	flat = np.array(gray)
+	scriptPath = os.path.dirname(os.path.realpath(__file__))
+	octave = oct2py.Oct2Py()
+	octave.addpath(scriptPath)
+	octave.pkg('load', 'image')
+	
+	img = myread(file)
+	print img[3].shap4
 
-	plt.figure(1)
-    	plt.subplot(2, 1, 1)
-	plt.imshow(gray)
-	plt.subplot(2, 1, 2)
-	plt.hist(flat)
-	plt.show()
+
+	#img_cor = octave.histcor(img)
+	if 0:
+		plt.figure(1)
+		plt.imshow(img)
+		plt.show()
+	
+
+
+
+
+	
